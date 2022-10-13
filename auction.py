6e9ec66a822a9712dff91bf8ef8346e5e0259667
Huw Barber <huwbarber@gmail.com>
2022-10-13 19:09:11 +0100

    Implemented execute_round

--- auction.py
+++ auction.py
@@ -8,7 +8,19 @@ class Auction:
         self.balances = {bidder:0 for bidder in bidders}
 
     def execute_round(self):
-        
+        user = random.choice(self.users)
+        bids = np.empty((len(self.bidders),1))
+        for bidder in self.bidders:
+            bids = np.append(bids, bidder.bid(user))
+        winner = self.bidders[np.argmax(np.array(bids))]
+        second_price = bids.sort()[-2]
+        for bidder in self.bidders:
+            if bidder == winner:
+                clicked = user.show_ad()
+                bidder.notify(True, second_price, clicked)
+                self.balances[bidder] += (1-second_price) if clicked else -second_price
+            else:
+                bidder.notify(False, second_price, None)
 
 
 class User:
